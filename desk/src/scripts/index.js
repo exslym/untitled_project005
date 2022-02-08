10ab83b55020df4b39fd438def159672f58bf96b
exslym <76844562+exslym@users.noreply.github.com>
2022-02-08 18:55:19 +0300

    desktop/mobile animation activating method fixed

--- desk/src/scripts/index.js
+++ desk/src/scripts/index.js
@@ -1,4 +1,5 @@
 import '../styles/index.scss';
+import * as jQuery from '../../public/tools/jquery-3.6.0.min.js';
 
 if (process.env.NODE_ENV === 'development') {
 	require('../index.html');
@@ -6,4 +7,12 @@ if (process.env.NODE_ENV === 'development') {
 
 window.addEventListener('DOMContentLoaded', function () {
 	'use strict';
+	const noHover = matchMedia('(hover: none)').matches;
+	if (noHover) {
+		jQuery(function ($) {
+			$('.loader').on('touchstart', function () {
+				$(this).toggleClass('hover');
+			});
+		});
+	}
 });
