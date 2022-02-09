e403690fdf77b0f0781684dca870b2af475305bf
exslym <76844562+exslym@users.noreply.github.com>
2022-02-09 10:00:12 +0300

    jQuery import removed, vanilla JS only

--- desk/src/scripts/index.js
+++ desk/src/scripts/index.js
@@ -1,5 +1,5 @@
 import '../styles/index.scss';
-import * as jQuery from '../../public/tools/jquery-3.6.0.min.js';
+// import * as jQuery from '../../public/tools/jquery-3.6.0.min.js';
 
 if (process.env.NODE_ENV === 'development') {
 	require('../index.html');
@@ -7,12 +7,17 @@ if (process.env.NODE_ENV === 'development') {
 
 window.addEventListener('DOMContentLoaded', function () {
 	'use strict';
-	const noHover = matchMedia('(hover: none)').matches;
-	if (noHover) {
-		jQuery(function ($) {
-			$('.loader').on('touchstart', function () {
-				$(this).toggleClass('hover');
+	// const noHover = matchMedia('(hover: none)').matches;
+	if (matchMedia('(hover: none)').matches) {
+		document
+			.querySelector('.loader')
+			.addEventListener('touchstart', function () {
+				this.classList.toggle('hover');
 			});
-		});
+		// jQuery(function ($) {
+		// 	$('.loader').on('touchstart', function () {
+		// 		$(this).toggleClass('hover');
+		// 	});
+		// });
 	}
 });
